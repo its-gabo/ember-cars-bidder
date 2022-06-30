034d78ba7abee4d8c188c72c11a8736931deaefb
Gabriel <smolana.gabriel@gmail.com>
2022-06-30 17:18:38 +0200

    change if statement in login controller

--- app/login/controller.js
+++ app/login/controller.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
 
 export default class RegisterController extends Controller {
   @service store;
@@ -24,7 +25,7 @@ export default class RegisterController extends Controller {
   async onSubmit(event) {
     event.preventDefault();
 
-    if (this.nickname !== '' && this.password !== '') {
+    if (!isEmpty(this.nickname) && !isEmpty(this.password)) {
       this.session.loginUser(this.nickname, this.password);
     }
   }
