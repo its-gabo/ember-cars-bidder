a2c64da83fd3ce470507d568b0e0f4f5b572f90b
Gabriel <smolana.gabriel@gmail.com>
2022-07-01 13:20:24 +0200

    add offer functionality

--- app/offers/route.js
+++ app/offers/route.js
@@ -2,6 +2,17 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 export default class OffersRoute extends Route {
   @service store;
+  @service session;
+
+  get doOffersExist() {
+    return !!this.model.length;
+  }
+
+  beforeModel() {
+    const { isUserLoggedIn } = this.session;
+
+    return isUserLoggedIn;
+  }
 
   model() {
     return this.store.findAll('offer');
