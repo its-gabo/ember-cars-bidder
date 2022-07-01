78505724fd7cb0c9ef8718583fea0eba0218bf3c
Gabriel <smolana.gabriel@gmail.com>
2022-07-01 19:45:46 +0200

    update dashboard

--- app/dashboard/route.js
+++ app/dashboard/route.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class DashboardRoute extends Route {}
+export default class DashboardRoute extends Route {
+  @service store;
+
+  model() {
+    return this.store.findAll('offer');
+  }
+}
